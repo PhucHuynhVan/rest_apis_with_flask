dd53531d023115e28abb058fda6a24eeaea8b725
PhucHuynhVan <phuc.huynhvanspkt@gmail.com>
2023-02-07 23:24:47 +0700

    Flask: Finishing store with SQL and ORM Flask SQLAlchemy

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
-import os
-
 from flask import Flask
 from flask_smorest import Api
 
 from db import db
 
-# import models
-
-from resources.store import store_blp as StoreBlueprint
-from resources.item import item_blp as ItemBlueprint
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
 
 
 def create_app(db_url=None):
-
     app = Flask(__name__)
 
-    app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
@@ -26,19 +20,16 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
-        "DATABASE_URL", "sqlite:///data.db"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["PROPAGATE_EXCEPTIONS"] = True
     db.init_app(app)
-
     api = Api(app)
 
     with app.app_context():
         db.create_all()
 
-    api.register_blueprint(StoreBlueprint)
     api.register_blueprint(ItemBlueprint)
+    api.register_blueprint(StoreBlueprint)
 
     return app
--- resources/store.py
+++ resources/store.py
@@ -1,61 +1,48 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-function-docstring
-import uuid
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+
 from db import db
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.exc import SQLAlchemyError
 from models import StoreModel
-from schemas import StoreSchema, StoreUpdateSchema
-
-store_blp = Blueprint("Stores", __name__, description="Operations on store")
+from schemas import StoreSchema
 
 
-@store_blp.route("/store")
-class StoreList(MethodView):
-    """Method for Stores"""
-
-    @store_blp.response(200, StoreSchema(many=True))
-    def get(self):
-        return StoreModel.query.all()
-
-    @store_blp.arguments(StoreSchema)
-    @store_blp.response(201, StoreSchema)
-    def post(self, store_date):
-        store = StoreModel(**store_date)
-        try:
-            db.session.add(store)
-            db.session.commit()
-        except IntegrityError:
-            abort(400, message="A store with that name already exists")
-        except SQLAlchemyError:
-            abort(500, message="An error occurred while creating the store")
-        return store
+blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 
-@store_blp.route("/store/<string:store_id>")
+@blp.route("/store/<string:store_id>")
 class Store(MethodView):
-    """Method for individual store"""
-
-    @store_blp.response(200, StoreSchema)
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
         return store
 
-    @store_blp.arguments(StoreUpdateSchema)
-    @store_blp.response(200, StoreSchema)
-    def put(self, store_data, store_id):
-        store = StoreModel.query.get_or_404(store_id)
-        if store:
-            store.name = store_data["name"]
-        else:
-            store = StoreModel(id=StoreModel, **store_data)
-
-        db.session.add(store)
-        db.session.commit()
-        return store
-
     def delete(self, store_id):
         store = StoreModel.query.get_or_404(store_id)
-        raise NotImplementedError("Deleting an item is not implemented.")
+        db.session.delete(store)
+        db.session.commit()
+        return {"message": "Store deleted"}, 200
+
+
+@blp.route("/store")
+class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True))
+    def get(self):
+        return StoreModel.query.all()
+
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(self, store_data):
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                message="A store with that name already exists.",
+            )
+        except SQLAlchemyError:
+            abort(500, message="An error occurred creating the store.")
+
+        return store
