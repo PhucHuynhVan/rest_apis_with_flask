f36247f0579163907ce02a1393600a8142b61621
PhucHuynhVan <phuc.huynhvanspkt@gmail.com>
2023-02-08 00:21:54 +0700

    Flask: Add tag APIs

--- schemas.py
+++ schemas.py
@@ -1,44 +1,37 @@
-# pylint: disable=missing-module-docstring
 from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    """Schemas for individual Item"""
-
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
 
 
-class ItemUpdateSchema(Schema):
-    """Schemas for update individual Item"""
-
-    name = fields.Str()
-    price = fields.Float()
-    store_id = fields.Int()
-
-
 class PlainStoreSchema(Schema):
-    """Schemas for individual Store"""
-
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 
-class StoreUpdateSchema(Schema):
-    """Schemas for update individual Store"""
-
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
     name = fields.Str()
 
 
 class ItemSchema(PlainItemSchema):
-    """Schemas for individual Item"""
-
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
 
-class StoreSchema(PlainStoreSchema):
-    """Schemas for individual Store"""
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
 
+
+class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+class TagSchema(PlainItemSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
